b5f67dd1224f1d5109468eaaaeef25056b9e0641
joeshaker <youssefshaker296@gmail.com>
2025-04-27 16:49:20 +0300

    BasketController

--- Core/Service/ServiceManager.cs
+++ Core/Service/ServiceManager.cs
@@ -9,10 +9,15 @@ using ServiceAbstraction;
 
 namespace Service
 {
-    public class ServiceManager(IUnitOfWork unitOfWork ,IMapper mapper) : IServiceManager
+    public class ServiceManager(IUnitOfWork unitOfWork ,IMapper mapper,IBasketRepository basketRepository) : IServiceManager
     {
         private readonly Lazy<IProductService> _LazyProductService = new Lazy<IProductService>(() =>
         new ProductService(unitOfWork,mapper));
         public IProductService ProductService => _LazyProductService.Value;
+
+        private readonly Lazy<IBasketService> _LazyBasketService = new Lazy<IBasketService>(() =>
+        new BasketService(basketRepository, mapper));
+
+        public IBasketService BasketService => _LazyBasketService.Value;
     }
 }
---
+++ Infrastructure/Presentation/Controllers/BasketController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ServiceAbstraction;
+using Shared.DataTransferObject.BasketModuleDto;
+
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class BasketController(IServiceManager serviceManager) : ControllerBase
+    {
+        [HttpGet]
+
+        public async Task<ActionResult<BasketDto>> GetBasket(string key)
+        {
+            var Basket = await serviceManager.BasketService.GetBasketAsync(key);
+            return Ok(Basket);
+        }
+
+        [HttpPost]
+
+        public async Task<ActionResult<BasketDto>> CreateOrUpdateBasket(BasketDto basket)
+        {
+            var Basket = await serviceManager.BasketService.CreateOrUpdateBasketAsync(basket);
+            return Ok(Basket);
+        }
+
+        [HttpDelete("{key}")]
+
+        public async Task<ActionResult<bool>> DeleteBasket(string key)
+        {
+             var Result = await serviceManager.BasketService.DeleteBasketAsync(key);
+            return Ok(Result);
+        }
+
+    }
+}
